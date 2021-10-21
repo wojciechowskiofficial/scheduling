8a144e7713a43026af60d904ec7c9f215de9b9c5
Adam Wojciechowski <adi61044@gmail.com>
2021-10-21 23:05:22 +0200

    new generator, new instances

--- zad1/gen_136376.py
+++ zad1/gen_136376.py
@@ -9,23 +9,26 @@ def Main():
     args = parser.parse_args()
     # generate
     n = args.n
-    magnitude = np.random.randint(100, 1000)
-    p_transposed = np.random.randint(round(magnitude / 2), round(magnitude * 3 / 2), n)
-    r_transposed = np.random.chisquare(df=5, size=n)
-    r_transposed = np.where(r_transposed > 16, np.random.randint(1, 16), r_transposed)
+    # sample ps from uniform(a, b) distribution
+    p_a = 100
+    p_b = 1000
+    p_transposed = np.random.randint(p_a, p_b, n)
+    # sample rs from chi-square distribution shifted by constant 
+    r_b = np.sum(p_transposed) * .75
+    chi2_scale = r_b / 16
+    r_transposed = chi2.rvs(df=5, scale=chi2_scale, size=n)
+    # upper-boudnd right tail
+    r_transposed = np.where(r_transposed > 16 * chi2_scale, np.random.randint(1, 16 * chi2_scale), r_transposed)
     r_transposed = np.rint(r_transposed)
-    sum_of_p = np.sum(p_transposed)
-    max_of_r = np.max(r_transposed)
-    scaler = round(sum_of_p / max_of_r)
-    r_transposed *= np.random.randint(1, scaler, n)
     r_transposed = r_transposed.astype(np.int64)
+    # sample ds from uniform(p + r, (p + r) + p_b - p_a)
     r_plus_p_transposed = r_transposed + p_transposed
     d_transposed = r_plus_p_transposed
     for i in range(d_transposed.shape[0]):
-        d_transposed[i] = np.random.randint(d_transposed[i], d_transposed[i] + magnitude)
+        d_transposed[i] = np.random.randint(d_transposed[i] + p_a, d_transposed[i] + p_b)
     prd_matrix = np.array([p_transposed, r_transposed, d_transposed], dtype=np.int64)
     prd_matrix = np.transpose(prd_matrix)
-    s_matrix = np.random.randint(round(np.min(p_transposed) * .1), np.min(p_transposed), size=(n, n))
+    s_matrix = np.random.randint(np.min(p_transposed), np.round(np.max(p_transposed) / 2), size=(n, n))
     prd_matrix[0][1] = 0
     with open('in_136376_' + str(n) + '.txt', 'w') as f:
         f.write(str(n))
--- zad1/ver.sh
+++ zad1/ver.sh
@@ -2,5 +2,5 @@
 
 args=("$@")
 for ((i=50; i<=500; i+= 50)); do
-	python ver_136376.py --in_name "${args[0]}/in_${args[0]}_$i.txt" --out_name "out_136376_$i.txt"
+	python ver_136376.py --in_name "${args[0]}/in_${args[0]}_$i.txt" --out_name "OUT/out_136376_$i.txt"
 done
