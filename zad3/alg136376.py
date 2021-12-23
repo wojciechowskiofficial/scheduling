64811e619b1293d06c96ecb8eb7464eda5bb53c5
Adam Wojciechowski <adi61044@gmail.com>
2021-12-23 06:24:04 +0100

    alg prob working, no file write

--- zad3/alg136376.py
+++ zad3/alg136376.py
@@ -11,12 +11,15 @@ def Main():
         one_string_in = f.read().split()
     gen_one_string_in = iter(one_string_in)
     n = int(next(gen_one_string_in))
-    data = np.empty(shape=(n, 7), dtype=np.int64)
+    data = np.empty(shape=(n, 8), dtype=np.int64)
     for i in range(n):
         for j in range(7):
             data[i, j] = int(next(gen_one_string_in))
     # compute mean operation duration
     mean_op = int(np.mean(np.sum(data[:,:4], axis=1)))
+    #   add ids as last column
+    for i in range(n):
+        data[i,-1] = i
     # assign initial task
     #   choose first task
     task_list = list()
@@ -52,43 +55,66 @@ def Main():
                 m_curr[k] = m[k]
             m_curr[0] += task_list[j][0]
             for k in range(1, 4, 1):
-                m_curr[k] = max(m[k - 1], m_curr[k]) + task_list[j][k]
+                m_curr[k] = max(m_curr[k - 1], m_curr[k]) + task_list[j][k]
             #   if after adding mean_op still befor it's d add to neglectable
             if m_curr[3] + mean_op < task_list[j][4]:
+                # print(m_curr[3] + mean_op, task_list[j][4])
                 neglectable.append(task_list[j])
             #   if after adding mean_op task goes from being to early 
             #   to being to late add to prioritized
             else:
                 prioritized.append(task_list[j])
-
-        print(prioritized)
-        print(neglectable)
-        print(len(prioritized) + len(neglectable))
-
-
-
-
-
-    # load the scheduling output file
-    # with open(sys.argv[2], 'r') as f:
-    #     one_string_in = f.read().split()
-    # gen_one_string_in = iter(one_string_in)
-    # obj_given = int(next(gen_one_string_in))
-    # sch = np.empty(shape=n, dtype=np.int64)
-    # for i in range(n):
-    #     sch[i] = int(next(gen_one_string_in))
-    # validate
-    # obj_computed = 0
-    # m[0] = data[sch[0], 0]
-    # for i in range(1, 4, 1):
-    #     m[i] = m[i - 1] + data[sch[0], i]
-    # obj_computed += compute_singular_obj(m[3], data[sch[0], 4], data[sch[0], 5], data[sch[0], 6])
-    # for i in range(1, n, 1):
-    #     m[0] += data[sch[i], 0]
-    #     for j in range(1, 4, 1):
-    #         m[j] = max(m[j - 1], m[j]) + data[sch[i], j]
-    #     obj_computed += compute_singular_obj(m[3], data[sch[i], 4], data[sch[i], 5], data[sch[i], 6])
-    # print(obj_computed)
+        #   if after adding mean_op no task would be late
+        if len(prioritized) > 0:
+            max_obj = 0
+            max_j = 0
+            max_m = None
+            #   find least early task
+            for j in range(len(prioritized)):
+                m_curr = np.zeros(shape=4, dtype=np.int64)
+                for k in range(4):
+                    m_curr[k] = m[k]
+                m_curr[0] += prioritized[j][0]
+                for k in range(1, 4, 1):
+                    m_curr[k] = max(m[k - 1], m_curr[k]) + prioritized[j][k]
+                if compute_singular_obj(m_curr[3], prioritized[j][4], prioritized[j][5], prioritized[j][6]) > max_obj:
+                    max_obj = compute_singular_obj(m_curr[3], prioritized[j][4], prioritized[j][5], prioritized[j][6]) > max_obj
+                    max_j = j
+                    max_m = m_curr
+            scheduling.append(prioritized.pop(max_j)[-1])
+            m = max_m
+        else:
+            min_obj = 0
+            min_j = 0
+            min_m = None
+            #   find least early task
+            for j in range(len(neglectable)):
+                m_curr = np.zeros(shape=4, dtype=np.int64)
+                for k in range(4):
+                    m_curr[k] = m[k]
+                m_curr[0] += neglectable[j][0]
+                for k in range(1, 4, 1):
+                    m_curr[k] = min(m[k - 1], m_curr[k]) + neglectable[j][k]
+                if compute_singular_obj(m_curr[3], neglectable[j][4], neglectable[j][5], neglectable[j][6]) > min_obj:
+                    min_obj = compute_singular_obj(m_curr[3], neglectable[j][4], neglectable[j][5], neglectable[j][6]) > min_obj
+                    min_j = j
+                    min_m = m_curr
+            scheduling.append(neglectable.pop(min_j)[-1])
+            m = min_m
+        task_list = prioritized + neglectable
+    # compute_obj
+    obj_computed = 0
+    m = np.zeros(shape=4, dtype=np.int64)
+    m[0] = data[scheduling[0], 0]
+    for i in range(1, 4, 1):
+        m[i] = m[i - 1] + data[scheduling[0], i]
+    obj_computed += compute_singular_obj(m[3], data[scheduling[0], 4], data[scheduling[0], 5], data[scheduling[0], 6])
+    for i in range(1, n, 1):
+        m[0] += data[scheduling[i], 0]
+        for j in range(1, 4, 1):
+            m[j] = max(m[j - 1], m[j]) + data[scheduling[i], j]
+        obj_computed += compute_singular_obj(m[3], data[scheduling[i], 4], data[scheduling[i], 5], data[scheduling[i], 6])
+    print(obj_computed)
 
 if __name__ == '__main__':
     Main()
